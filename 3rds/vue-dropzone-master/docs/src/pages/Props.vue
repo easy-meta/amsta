c368dbfe84b28678002a29b0174d4b7b79eb0485
Larry Kwan <i@gyz.me>
2025-11-08 19:48:36 +0800

    add article static files

---
+++ 3rds/vue-dropzone-master/docs/src/pages/Props.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Props</h1>
+    <p>The following props are available for the Vue2-Dropzone component.</p>
+    <simple-table :headers="['Prop Name', 'Type', 'Description', 'Required']" :rows="props" />
+    <edit-doc :link="'Props.vue'"></edit-doc>
+  </div>
+</template>
+
+<script>
+import SimpleTable from '../components/SimpleTable.vue';
+import editDoc from '../components/DocEditLink.vue';
+
+export default {
+  data: function() {
+    return {
+      props: [
+        ['id', 'String', 'A string by which to identify the component, can be anything', 'True'],
+        ['options', 'Object', 'A dropzone [configuration object](http://www.dropzonejs.com/#configuration-options), accepts all valid dropzone configuration', 'True'],
+        ['includeStyling', 'Boolean', 'Whether to include the dropzone and component styling, defaults to true', 'False'],
+        ['awss3', 'Object', 'Object consisting of 3 values signingURL, headers, and params. You can use the headers and params keys to send additional headers or parameters with the signing request (e.g. CSRF tokens). See [Demo and config](#/aws-s3-upload)', 'False'],
+        ['destroyDropzone', 'Boolean', 'Destroy the dropzone object when the component is destroyed. Defaults to true.', 'False'],
+      ]
+    }
+  },
+  components: {
+    'simple-table': SimpleTable,
+    'edit-doc': editDoc
+  }
+}
+</script>
+
---
+++ 3rds/vue-dropzone-master/src/services/urlsigner.js
@@ -0,0 +1,77 @@
+export default {
+  getSignedURL(file, config) {
+    let payload = {
+      filePath: file.name,
+      contentType: file.type
+    }
+
+    return new Promise((resolve, reject) => {
+      var fd = new FormData();
+      let request = new XMLHttpRequest();
+      request.open("POST", config.signingURL);
+      request.onload = function () {
+        if (request.status == 200) {
+          resolve(JSON.parse(request.response));
+        } else {
+          reject((request.statusText));
+        }
+      };
+      request.onerror = function (err) {
+        console.error("Network Error : Could not send request to AWS (Maybe CORS errors)");
+        reject(err)
+      };
+      Object.entries(config.headers).forEach(([name, value]) => {
+        request.setRequestHeader(name, value);
+      });
+      Object.entries(config.params).forEach(([name, value]) => {
+        fd.append(name, value);
+      });
+
+      request.send(fd);
+    });
+  },
+  sendFile(file, config) {
+    var fd = new FormData();
+    return this.getSignedURL(file, config)
+      .then((response) => {
+        let signature = response.signature;
+        Object.keys(signature).forEach(function (key) {
+          fd.append(key, signature[key]);
+        });
+        fd.append('file', file);
+        return new Promise((resolve, reject) => {
+          let request = new XMLHttpRequest();
+          request.open('POST', response.postEndpoint);
+          request.onload = function () {
+            if (request.status == 201) {
+              var s3Error = (new window.DOMParser()).parseFromString(request.response, "text/xml");
+              var successMsg = s3Error.firstChild.children[0].innerHTML;
+              resolve({
+                'success': true,
+                'message': successMsg
+              })
+            } else {
+              var s3Error = (new window.DOMParser()).parseFromString(request.response, "text/xml");
+              var errMsg = s3Error.firstChild.children[0].innerHTML;
+              reject({
+                'success': false,
+                'message': errMsg + ". Request is marked as resolved when returns as status 201"
+              })
+            }
+          };
+          request.onerror = function (err) {
+            var s3Error = (new window.DOMParser()).parseFromString(request.response, "text/xml");
+            var errMsg = s3Error.firstChild.children[1].innerHTML;
+            reject({
+              'success': false,
+              'message': errMsg
+            })
+          };
+          request.send(fd);
+        });
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+}
